8167cb788fcc427bf04c94377645d304292ac981
y-moriguchi <yuichiro-moriguchi@nifty.com>
2017-03-04 06:00:43 +0900

    supports instruction putstatic

---
+++ syaro/src/net/morilib/syaro/classfile/code/Putstatic.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2016-2017 Yuichiro Moriguchi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.morilib.syaro.classfile.code;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import net.morilib.syaro.classfile.ConstantFieldref;
+import net.morilib.syaro.classfile.GatheredConstantPool;
+import net.morilib.syaro.classfile.Mnemonic;
+
+/**
+ * This class represents an Java VM instruction putfield.
+ * 
+ * @author Yuichiro MORIGUCHI
+ */
+public class Putstatic extends Mnemonic {
+
+	private ConstantFieldref field;
+
+	/**
+	 * constructs a putfield.
+	 * 
+	 * @param field field reference
+	 */
+	public Putstatic(ConstantFieldref field) {
+		super(179);
+		this.field = field;
+	}
+
+	@Override
+	public void gatherConstantPool(GatheredConstantPool gathered) {
+		field.gatherConstantPool(gathered);
+	}
+
+	@Override
+	protected void generateMnemonicCode(GatheredConstantPool gathered,
+			DataOutputStream ous) throws IOException {
+		ous.writeShort(gathered.getIndex(field));
+	}
+
+	@Override
+	protected int getByteLength() {
+		return 3;
+	}
+
+}
