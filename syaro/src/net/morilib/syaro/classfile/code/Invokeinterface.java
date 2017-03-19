7ca2b028654385151ff96b0ed9c9f6e835f479a9
y-moriguchi <yuichiro-moriguchi@nifty.com>
2017-03-19 09:30:31 +0900

    add instruction invokeinterface

---
+++ syaro/src/net/morilib/syaro/classfile/code/Invokeinterface.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016-2017 Yuichiro Moriguchi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.morilib.syaro.classfile.code;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import net.morilib.syaro.classfile.ConstantMethodref;
+import net.morilib.syaro.classfile.GatheredConstantPool;
+import net.morilib.syaro.classfile.Mnemonic;
+
+/**
+ * This class represents an Java VM instruction invokeinterface.
+ * 
+ * @author Yuichiro MORIGUCHI
+ */
+public class Invokeinterface extends Mnemonic {
+
+	private ConstantMethodref method;
+
+	/**
+	 * constructs a invokeinterface.
+	 * 
+	 * @param method method reference
+	 */
+	public Invokeinterface(ConstantMethodref method) {
+		super(185);
+		this.method = method;
+	}
+
+	@Override
+	public void gatherConstantPool(GatheredConstantPool gathered) {
+		method.gatherConstantPool(gathered);
+	}
+
+	@Override
+	protected void generateMnemonicCode(GatheredConstantPool gathered,
+			DataOutputStream ous) throws IOException {
+		ous.writeShort(gathered.getIndex(method));
+		ous.writeByte(72);
+		ous.writeByte(0);
+	}
+
+	@Override
+	protected int getByteLength() {
+		return 5;
+	}
+
+}
